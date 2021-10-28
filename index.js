07cb7df1eca40bc2b213d88ea76ed66b886239c9
mandemt <70643363+mandemt@users.noreply.github.com>
2021-10-28 13:17:40 +0200

    nu local op de server van MAMP. Daarnaast een werkende methode om de antwoorden uit de dataset te verwerken en dus door drie verschillende functies te laten lopen.

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
-const express = require('express'); // express wordt gebruikt
-const app = express(); 
-const port = 4000; // met localhost:3000 bezoek je de server in de browser
+fetch('http://localhost:8888/tt-dataset.json') // laadt het JSON bestand in
+.then(data => data.json())
+.then(data => {
+	return data.map(obj => {
+		Object.keys(obj).forEach(key => {
+			obj[key] = removeSpaces(obj[key]);
+			obj[key] = removeCapitals(obj[key]);
+			obj[key] = removeEmptyValues(obj[key])
+			obj[key] = bekijkAlles(obj[key])
 
-
-let dataset = require('./tt-dataset.json')
-app.use(express.static('static')) // nu worden er static files opgevraagd vanuit een andere map.
-app.set('view engine', 'ejs') // instellen voor view engine
-let ejs = require('ejs');
-
-var vragen = Object.keys(dataset[1]) // Script gaat over alle vraagelementen uit het JSON bestand.
-var antwoorden = Object.values(dataset[1])
-
-var hallo = Object.keys(dataset[1])
-// console.log(hallo)
-
-dataset.forEach((element, index) => {
-    var vragen = Object.values(dataset[index]) // Script gaat over alle vraagelementen uit het JSON bestand.
-console.log(vragen)
-    if (vragen[index] == 'Wat is je oogkleur?'){
-        var oogkleurvraag = vragen[index]
-        console.log(oogkleurvraag)
-    //    console.log('ja') // Als de vraag tegenkomt , wordt ja gezegd.
-    }
-    else{
-        var wrongQuestion = vragen[index]
-        // console.log(wrongQuestion) // Als hij hem niet tegenkomt, wordt de vraag gezegd
-    }
-})
-const cleanVariables = { b: 4, c: 5 };
-
-app.get('/', (req,res) => {
-	res.render('index',
-    {vragen, antwoorden, dataset},
- )
-
+		})
+	})
 })
 
-app.listen(port, () =>{
-	console.log('de app lusitert op localhost:', port)
-})
+	function removeSpaces(string) {	
+		if(typeof string === 'string'){
+			return string.replace(/ /gi, '');
+		}
+		else{
+			return string
+		}
+
+	}
+
+	function removeCapitals(string) {	
+
+		if(typeof string === 'string'){
+			
+			return string.toLowerCase();
+		}
+		else{
+			return string
+		}
+
+	}
+
+	function removeEmptyValues(string) {
+		// console.log(string)
+		if(string.length == 0 || string === 'geenidee' || string === 'maaktmijnietuit'){
+			return string.replace(string, 'no data')
+		}		
+		else{
+			return string;
+		}
+		
+	}
+	
+	function bekijkAlles(string){
+		console.log(string)
+	}
