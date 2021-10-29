30596508a932dffa656ca882e605b457b0898088
mandemt <70643363+mandemt@users.noreply.github.com>
2021-10-29 11:32:31 +0200

    Heb er nu voor gezorgd dat je een vraag kan intypen in de input, die daarna de resultaten (opgeschoond) presenteert op de website

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+function doeWat() {
 fetch('http://localhost:8888/tt-dataset.json') // laadt het JSON bestand in
 .then(data => data.json())
 .then(data => {
 	return data.map(data => {	
-		// console.log(data['Wat is je oogkleur?'])
-		woord = removeSpaces(data['Op welke verdieping van het TTH studeer je het liefst?']);
+		woord = removeSpaces(data[vulin.value]);
 		variabele = removeCapitals(woord);
-		volledig = removeEmptyValues(variabele);
-		console.log(volledig)
+		volledig = changeStringToNumber(variabele);
+		let lijst = document.createElement("li")
+		lijst.textContent = volledig
+		document.body.appendChild(lijst)
+
+		
 	})
 })
+}
 
+// vulin.addEventListener('change',roepNaam )
+let vulin = document.querySelector("input[type='text']")
+let stuur = document.querySelector("input[type='submit']")
+let tekst = document.querySelector("ul  ")
+stuur.addEventListener('click', doeWat)
 	function removeSpaces(string) {	
 		if(typeof string === 'string'){
 			return string.replace(/ |[.]|/gi, '');
@@ -32,19 +42,27 @@ fetch('http://localhost:8888/tt-dataset.json') // laadt het JSON bestand in
 
 	}
 
-	function removeEmptyValues(string) {
+	function changeStringToNumber(string) {
 
-		if(string === 'geen' | string === 'thuis' | string === 'niet') {
+		if(string === 'geen' | string === 'thuis' | string === 'niet') { // oplossen foute data
 			return string.replace(string, 'no data')
-		}		
-		if(string === '3everdieping' | string === '3e'){
-		return string.replace(string, 3) // moet nog van string naar number gaan
-		
-		
+		}	
+
+		if( string ==='eerste') { // oplossen synoniemen en variaties voor 1
+			string = string.replace(string, 1) // Het synoniem wordt vervangen door de content 3
+			return Number(string)	// het datatype gaat van string naar Number
 		}
-		if(string === '2deverdieping' | string === 'medialounge'){
-			return string.replace(string,2) // moet nog van string naar number gaan
+		
+		if(string === '2deverdieping' | string === 'medialounge'){ 
+			string = string.replace(string, 2)
+			return Number(string) 
 		}
+
+		if(string === '3everdieping' | string === '3e' | string === 'debovenste'){ 
+			string = string.replace(string, 3) 
+			return Number(string) 
+		}
+
 		else{
 			return string;
 		}	
