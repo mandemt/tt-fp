caff8a10f96ea315cb1bd246e97d8d336f9769a0
mandemt <70643363+mandemt@users.noreply.github.com>
2021-11-02 13:51:27 +0100

    Je kunt nu een ID van een kunstwerk naar keuze invullen. De browser keert 7 voorkomende kleuren in het schilderij terug.

--- API/index2.js
+++ API/index2.js
@@ -1,22 +1,38 @@
-let vulin = 'BK-1977-67'
-let invulform = document.querySelector('input[type="text"]')
-var antwoord = invulform.value
-var request = new XMLHttpRequest(); // de aanvraag naar de data van de API wordt aangevraagd
-request.open('GET', 'https://www.rijksmuseum.nl/api/nl/collection/?key=' + apikey + '&involvedMaker=' + antwoord )
+const dinges = document.querySelector('input[type="text"]')
+dinges.addEventListener('input', veranderWaarde)
 
-request.onload = function() {
-    var data = JSON.parse(request.response)
-console.log(data)
-   const kunst = data.artObjects
-   kunst.forEach (kunst => {
-       console.table(kunst.title +' ' + kunst.webImage.url + ' id:' + kunst.id)
-   })
-
-      
-
-  
+function veranderWaarde() {
+   let id = dinges.value
+   maakReq(id)
 }
 
+function maakReq(id) {
+  
+
+
+var request = new XMLHttpRequest(); // de aanvraag naar de data van de API wordt aangevraagd
+request.open('GET', 'https://www.rijksmuseum.nl/api/nl/collection/' + id + '?key=' + apikey )
+
+request.onload = function() {
+
+let data = JSON.parse(request.response)
+let kleuren = data.artObject.colors
+let titel = data.artObject.title
+console.log(titel)
+
+
+if (request.status >= 200 && request.status < 400) {
+kleuren.forEach((kleur, index) => {
+    let lijst = document.getElementById(index)
+    lijst.style.backgroundColor = kleur.hex
+    
+    
+})
+}
+else{
+    console.log('dit kunstwerk bestaat niet!')
+}
+let naam = document.getElementsByClassName('hallo')
+}
 request.send()
-
-
+    }
