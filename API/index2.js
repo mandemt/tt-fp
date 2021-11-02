a3062d7ef0431031ed268b50508e6dc26c174702
mandemt <70643363+mandemt@users.noreply.github.com>
2021-11-02 12:58:31 +0100

    dit script kan de waarde in het invulformulier gebruiken om een collectie van een bepaalde kunstenaar te laden en deze in de console.log te presenteren als titel, afbeelding en kunst id.

--- API/index2.js
+++ API/index2.js
@@ -1,12 +1,20 @@
-let apikey = 'N7PX59OF'
-
+let vulin = 'BK-1977-67'
+let invulform = document.querySelector('input[type="text"]')
+var antwoord = invulform.value
 var request = new XMLHttpRequest(); // de aanvraag naar de data van de API wordt aangevraagd
-request.open('GET', 'https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=' + apikey)
+request.open('GET', 'https://www.rijksmuseum.nl/api/nl/collection/?key=' + apikey + '&involvedMaker=' + antwoord )
 
 request.onload = function() {
-    var data = JSON.parse(this.response)
+    var data = JSON.parse(request.response)
+console.log(data)
+   const kunst = data.artObjects
+   kunst.forEach (kunst => {
+       console.table(kunst.title +' ' + kunst.webImage.url + ' id:' + kunst.id)
+   })
 
-   console.log(data.artObject.colors)
+      
+
+  
 }
 
 request.send()
