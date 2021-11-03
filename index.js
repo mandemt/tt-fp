457d50ebc3ca4a3d662ed574b9f04a4f9b47ec4f
mandemt <70643363+mandemt@users.noreply.github.com>
2021-11-03 11:37:55 +0100

    clean coding: semicolons, indents en naamgeving

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-const dinges = document.querySelector('input[type="text"]')
-const sturen = document.querySelector('input[type="submit"]')
-sturen.addEventListener('click', veranderWaarde)
+const textInput = document.querySelector('input[type="text"]')
+const submitInput = document.querySelector('input[type="submit"]')
+submitInput.addEventListener('click', veranderWaarde)
 
-			
-	function veranderWaarde() {
-			   let key = dinges.value
-			   cleanData(key)
-			}
-			
-function cleanData(key) {
+function veranderWaarde() {
+	let vraag = textInput.value;
+	cleanData(vraag);
+}
+
+function cleanData(vraag) {
 	fetch('http://localhost:8888/tt-dataset.json') // laadt het JSON bestand in
 		.then(data => data.json())
-		.then(data => {	
-			let oud = []
-			let array = []
-			data.map(data => {
-			
-				sturen.addEventListener('submit', veranderWaarde)
-				oud.push(data[key])
+		.then(data => {
+			let oud = [];
+			let array = [];
 
-				onbewerkt = verwijderSpaties(data[key]); // Onbewerkte antwoorden worden toegankelijkere waarden zonder spaties
+			data.map(data => {
+				oud.push(data[vraag]);
+				onbewerkt = verwijderSpaties(data[vraag]); // Onbewerkte antwoorden worden toegankelijkere waarden zonder spaties
 				waarde = verwijderHoofdletters(onbewerkt); // waarden worden zonder hoofdletter voor meer consistentie
 				volledig = voorkomDubbeleWaarden(waarde); // De verdiepingen worden vervangen door een getal. Ook wordt het type data Number.
 				ultimate = stringNaarNummer(volledig);
-				array.push(ultimate)
+				array.push(ultimate);
+			})
 
-							})
+			function sorteren(waarde1, waarde2) { // sort() werkt op alfabetische volgorde, dus 1  == 10 waardoor deze bij elkaar komen te staan
+				if (waarde1 < waarde2) { // Nu zal de array data gezien worden als een getal en dus wel in volgorde worden gezet
+					return -1;
+				}
+			}
 
-								function sorteren(waarde1, waarde2) { // sort() werkt op alfabetische volgorde, dus 1  == 10 waardoor deze bij elkaar komen te staan
-								if (waarde1 < waarde2){ // Nu zal de array data gezien worden als een getal en dus wel in volgorde worden gezet
-								return -1; 
-								}
-							  }
-							
-
-		console.table(oud)
-		
-		console.table(array.sort(sorteren))
-	
-	})
-		
-		
+			console.table(oud);
+			console.table(array.sort(sorteren));
+		})
 }
 
 function verwijderSpaties(string) {
-	 if(typeof string === 'string'){
-	return string.replace(/ |[^a-z,0-9]|/gi, ''); // woorden vallen achter elkaar doordat leestekens vervangen worden door -niets-
-}
-
-else{
-	return string
-}
+	if (typeof string === 'string') {
+		return string.replace(/ |[^a-z,0-9]|/gi, ''); // woorden vallen achter elkaar doordat leestekens vervangen worden door -niets-
+	}
+	else {
+		return string;
+	}
 }
 
 function verwijderHoofdletters(string) {
@@ -60,36 +50,33 @@ function verwijderHoofdletters(string) {
 	else {
 		return string;
 	}
-
 }
 
 
 function voorkomDubbeleWaarden(string) {
-	if(typeof string === 'string'){
-	if (string.indexOf('frontend') > -1 | string.indexOf('front-end') > -1) {
-		return string.replace(string, 'frontender');
+	if (typeof string === 'string') {
+		if (string.indexOf('frontend') > -1 | string.indexOf('front-end') > -1) {
+			return string.replace(string, 'frontender');
+		}
+		if (string.indexOf('design') > -1) {
+			return string.replace(string, 'designer');
+		}
+		if (string === 'kerst') {
+			return string.replace(string, '25 december');
+		}
+		else {
+			return string;
+		}
 	}
-	if (string.indexOf('design') > -1) {
-		return string.replace(string, 'designer')
+	else {
+		return string;
 	}
-	if(string === 'kerst'){
-
-		let today = new Date();
-		return string.replace(string, '25 december')
-	}
-	else{
-		return string
-	}
-}	
-else {
-	return string
-}
 }
 
 
 function stringNaarNummer(string) {
 	if (string === 'geen' | string === 'thuis' | string === 'niet' | string === "" | string === "geenidee" | string === "ikbenlactoseintolerant,gaweg") { // lege waarden wordt null
-		return 'geen data'
+		return 'geen data';
 	}
 	if (string === 'eerste') { // oplossen synoniemen en variaties voor 1
 		string = string.replace(string, 1); // Het synoniem wordt vervangen door de content 3
@@ -106,7 +93,4 @@ function stringNaarNummer(string) {
 	else {
 		return string;
 	}
-
-
-
 }
